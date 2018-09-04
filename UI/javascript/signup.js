0e8e1c715332c9eaf83594ed3fe7916ce7715666
EvelyneNamwoyo <namwoyoeve@gmail.com>
2018-09-04 14:25:22 +0300

    add javascript to show and hide the signup and signin forms

---
+++ UI/javascript/signup.js
@@ -0,0 +1,23 @@
+//Get the signup form modal
+signin = document.getElementById("")
+
+//functions to display signup form and signin form
+
+function signup_show(){
+    document.getElementById("signup_modal").style.display = "block";
+}
+
+function signup_hide(){
+    document.getElementById("signup_modal").style.display = "none";
+
+}
+
+function signin_show(){
+    document.getElementById("signin_modal").style.display = "block";
+
+}
+
+function signin_hide(){
+    document.getElementById("signin_modal").style.display = "none";
+
+}
