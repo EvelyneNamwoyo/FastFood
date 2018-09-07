8a50300bca0c5e1ec73325401f6f78cefe1ec11a
EvelyneNamwoyo <namwoyoeve@gmail.com>
2018-09-07 12:48:25 +0300

    Update javascript

---
+++ javascript/signup.js
@@ -0,0 +1,41 @@
+//Get the signup form modal
+signin = document.getElementById("")
+
+//functions to display signup form and signin form
+
+function signup_show(){
+    document.getElementById("signup_modal").style.display = "block";
+}
+
+function signup_hide(){
+    document.getElementById("signup_modal").style.display = "none";
+
+}
+
+function signin_show(){
+    document.getElementById("signin_modal").style.display = "block";
+
+}
+
+function signin_hide(){
+    document.getElementById("signin_modal").style.display = "none";
+
+}
+
+
+//Jvascript to show and hide the added items
+function show_chicken(){
+    document.getElementById("chicken").style.display = "block";
+}
+
+function show_chips(){
+    document.getElementById("chips").style.display = "block";
+}
+
+function hide_chicken(){
+    document.getElementById("chicken").style.display = "none";
+}
+
+function hide_chips(){
+    document.getElementById("chips").style.display = "none";
+}
