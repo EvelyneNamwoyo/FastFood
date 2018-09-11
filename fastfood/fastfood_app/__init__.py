43880df3a5dc9dd5e6286986b82996dd286c3cf6
EvelyneNamwoyo <namwoyoeve@gmail.com>
2018-09-11 15:19:49 +0300

    add data structure to store orders

---
+++ fastfood/fastfood_app/__init__.py
@@ -0,0 +1,3 @@
+from flask import Flask
+app = Flask(__name__)
+from fastfood_app import views
---
+++ fastfood/fastfood_app/views.py
@@ -0,0 +1,20 @@
+from fastfood_app import app
+orders = [
+    {'id': 0,
+     'Category': 'Main Course',
+     'owner': 'Vernor Vinge',
+     'Food Name':'Chicken Stew'
+     'Description': 'Nice and tasty food'
+     },
+    {'id': 2,
+     'Category': 'Main Course',
+     'owner': 'Erina Sis',
+     'Food Name':'Rice pilau'
+     'Description': 'Nice and tasty food'},
+    {'id': 3,
+     'Category': 'Drinks',
+     'owner': 'Sue Nalima',
+     'Food Name':'Mango Juice'
+     'Description': 'Made from the natural african mango'}
+]
+
