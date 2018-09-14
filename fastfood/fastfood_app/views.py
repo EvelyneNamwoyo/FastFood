99ca8181661b763f7b3d97798aa0bc919cfebc6d
EvelyneNamwoyo <namwoyoeve@gmail.com>
2018-09-14 17:08:03 +0300

    Make changes to the fuction to creaate update status order endpoint

--- fastfood/fastfood_app/views.py
+++ fastfood/fastfood_app/views.py
@@ -1,18 +1,18 @@
 from fastfood_app import app
 from flask import jsonify, abort, make_response, request
 orders = [
-    {'id': 0,
-     'Category': 'Main Course',
+    {'id': 1,
+     'category': 'Main Course',
      'owner': 'Vernor Vinge',
      'Food Name':'Chicken Stew',
      'Description': 'Nice and tasty food'},
     {'id': 2,
-     'Category': 'Main Course',
+     'category': 'Main Course',
      'owner': 'Erina Sis',
      'Food Name':'Rice pilau',
      'Description': 'Nice and tasty food'},
     {'id': 3,
-     'Category': 'Drinks',
+     'category': 'Drinks',
      'owner': 'Sue Nalima',
      'Food Name':'Mango Juice',
      'Description': 'Made from the natural african mango'}
@@ -52,10 +52,12 @@ def place_order():
 @app.route('/FastFood/api/v2/orders/<int:order_id>', methods=['PUT'])
 def update_order_status(order_id):
     order = [ order for order in orders if order['id'] == order_id]
-    if ((len(order)==0) or (not request.json) or ('category' in request.json and type(request.json['category']) != unicode) or ('owner' in request.json and type(request.json['owner']) != unicode) or ('Food Name' in request.json and type(request.json['Food Name']) != unicode) or ('Description' in request.json and type(request.json['Description']) != unicode)):
-        abort(400)
+    if ((len(order)==0) or (not request.json) or ('category' in request.json and type(request.json['category']) != str) or ('owner' in request.json and type(request.json['owner']) != str) or ('Food Name' in request.json and type(request.json['Food Name']) != str) or ('Description' in request.json and type(request.json['Description']) != str)):
+         abort(400)
+    #order[0]['category'] = request.json['category']
     order[0]['category'] = request.json.get('category', order[0]['category'])
+    #order[0]['owner'] = request.json.get('owner', order[0]['owner'])
     order[0]['owner'] = request.json.get('owner', order[0]['owner'])
-    order[0]['Food Name'] = request.json.get('Food Name', order[0]['Food Name'])
-    order[0]['Description'] = request.json.get('Description', order[0]['Description'])
+    # order[0]['Food Name'] = request.json.get('Food Name', order[0]['Food Name'])
+    # order[0]['Description'] = request.json.get('Description', order[0]['Description'])
     return jsonify({'order':order[0]})
