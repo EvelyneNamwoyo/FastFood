72302287e7231275ad0d14d56d10df9897522673
EvelyneNamwoyo <namwoyoeve@gmail.com>
2018-09-17 15:46:28 +0300

    Add a test function to retrieve a specific order

---
+++ fastfood/test_api_endpoints.py
@@ -0,0 +1,22 @@
+import unittest
+from fastfood_app import app
+from fastfood_app import views
+class EndpointTest(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+        
+    def tearDown(self):
+        pass
+    
+    def test_to_get_all_orders(self):
+        response = self.app.get('/FastFood/api/v2/orders')
+        self.assertEqual(response.status_code, 200)
+
+    def test_to_fetch_aspecific_order(self):
+        response = self.app.get('/FastFood/api/v2/orders/1')
+        self.assertEqual(response.status_code,200)
+
+    
+
+if __name__ == "__main__":
+    unittest.main()
