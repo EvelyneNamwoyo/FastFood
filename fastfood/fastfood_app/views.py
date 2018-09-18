3ce054feedddad3e0fc6b6bf1d6971e64a21a816
EvelyneNamwoyo <namwoyoeve@gmail.com>
2018-09-18 13:40:45 +0300

    Add validation to place_order function to pass tests

--- fastfood/fastfood_app/views.py
+++ fastfood/fastfood_app/views.py
@@ -3,30 +3,27 @@ from flask import jsonify, abort, make_response, request
 orders = [
     {'id': 0,
      'Category': 'Main Course',
-     'owner': 'Vernor Vinge',
+     'order status': False,
      'Food Name':'Chicken Stew',
      'Description': 'Nice and tasty food'},
     {'id': 2,
      'Category': 'Main Course',
-     'owner': 'Erina Sis',
+     'order status': False,
      'Food Name':'Rice pilau',
      'Description': 'Nice and tasty food'},
     {'id': 3,
      'Category': 'Drinks',
-     'owner': 'Sue Nalima',
+     'order status': False,
      'Food Name':'Mango Juice',
      'Description': 'Made from the natural african mango'}
 ]
-@app.errorhandler(404)
-def not_found(error):
-    return make_response(jsonify({'error': 'Not found'}), 404)
 
 @app.route('/FastFood/api/v2/orders', methods=['GET'])
 def get_orders():
     return jsonify({'orders':orders})
 
 #This endpoint gets a specific order
-@app.route('/FastFood/api/v2/orders/<int:order_id>', methods=['GET'])
+@app.route('/FastFood/api/v1/orders/<int:order_id>', methods=['GET'])
 def get_order(order_id):
     order = [ order for order in orders if order['id'] == order_id]
     if len(order) == 0:
@@ -34,18 +31,34 @@ def get_order(order_id):
     return jsonify({'order':order[0]})
 
 #Function for an endpoint to create new request
-@app.route('/FastFood/api/v2/orders', methods=['POST'])
+@app.route('/FastFood/api/v1/orders', methods=['POST'])
 def place_order():
-    if not request.json or not 'Food Name' in request.json:
-        abort(400)
-    order = {
-            'id':orders[-1]['id'] + 1,
-            'category':request.json['category'],
-            'owner': request.json['owner'],
-            'Food Name':request.json['Food Name'],
-            'Description':request.json['Description']
+    if not request.json :
+        return make_response(jsonify({'error_message': 'No parameters were passed'}), 404)
+    elif not 'Food Name' in request.json:
+        return make_response(jsonify({'error_message': 'Parameter Food Name missing'}), 404)
+        
+    elif not isinstance(request.json['Food Name'], str):
+        bad_request = {
+            "error": "Invalid order object",
+            "help_string":
+                "Request format should be {'category': 'Drinks or empty',"
+                "'Food Name': 'orange juice','Description': 'Mixture of lemon and tangerine' }"
             }
-    orders.append(order)
-    return jsonify({'order':order}), 201
+        return jsonify({'bad_request':bad_request}), 400
+
+    else:
+        order = {
+                'id':orders[-1]['id'] + 1,
+                'category':request.json.get('category', ""),
+                # 'category':request.json['category'],
+                'order status': False,
+                'Food Name':request.json['Food Name'],
+                'Description':request.json.get('Description', "")
+                # 'Description':request.json['Description']
+                }
+        orders.append(order)
+        return jsonify({'order':order}), 201
+
 
 
