6bba08d1254a1fb8b80f30f6ad6b876c82f6f55e
EvelyneNamwoyo <namwoyoeve@gmail.com>
2018-09-18 16:22:55 +0300

    Add user authentiication before status update

--- fastfood/fastfood_app/views.py
+++ fastfood/fastfood_app/views.py
@@ -1,23 +1,26 @@
 from fastfood_app import app
 from flask import jsonify, abort, make_response, request
+from flask_httpauth import HttpBasicAuth
+auth = HttpBasicAuth
 orders = [
     {
-     'id': 1,
-     'category': 'Main Course',
-     'order status': False,
-     'Food Name':'Chicken Stew',
-     'Description': 'Nice and tasty food'},
+        'id': 1,
+        'category': 'Main Course',
+        'order status': False,
+        'Food Name':'Chicken Stew',
+        'Description': 'Nice and tasty food'},
     {
         'id': 2,
         'category': 'Main Course',
-        'order': False,
+        'order status': False,
         'Food Name':'Rice pilau',
-     'Description': 'Nice and tasty food'},
-    {'id': 3,
-     'category': 'Drinks',
-     'order': False,
-     'Food Name':'Mango Juice',
-     'Description': 'Made from the natural african mango'}
+        'Description': 'Nice and tasty food'},
+
+    {   'id': 3,
+        'category': 'Drinks',
+        'order status': False,
+        'Food Name':'Mango Juice',
+        'Description': 'Made from the natural african mango'}
 ]
 
 @app.route('/FastFood/api/v2/orders', methods=['GET'])
@@ -40,7 +43,7 @@ def place_order():
     order = {
             'id':orders[-1]['id'] + 1,
             'category':request.json['category'],
-            'owner': request.json['owner'],
+            'order status': request.json['order status'],
             'Food Name':request.json['Food Name'],
             'Description':request.json['Description']
             }
@@ -48,13 +51,20 @@ def place_order():
     return jsonify({'order':order}), 201
 
 
-@app.route('/FastFood/api/v2/orders/<int:order_id>', methods=['PUT'])
+@app.route('/FastFood/api/v1/orders/<int:order_id>', methods=['PUT'])
+@auth.login_required
 def update_order_status(order_id):
     order = [ order for order in orders if order['id'] == order_id]
     if ((len(order)==0) or (not request.json) or ('category' in request.json and type(request.json['category']) != str) or ('order status' in request.json and type(request.json['order status']) != str) or ('Food Name' in request.json and type(request.json['Food Name']) != str) or ('Description' in request.json and type(request.json['Description']) != str)):
         jsonify({'error': 'Invalid innput'}), 404
     order[0]['category'] = request.json.get('category', order[0]['category'])
-    order[0]['owner'] = request.json.get('owner', order[0]['owner'])
+    order[0]['order status'] = request.json.get('order status', order[0]['order status'])
     order[0]['Food Name'] = request.json.get('Food Name', order[0]['Food Name'])
     order[0]['Description'] = request.json.get('Description', order[0]['Description'])
     return jsonify({'order':order[0]})
+
+@auth.get_password
+def get_password(username):
+    if username=='admin':
+        return 'Eva'
+    return make_response(jsonify({'error': 'Unauthorized access'}), 401)
