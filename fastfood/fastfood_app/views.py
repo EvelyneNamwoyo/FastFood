c388e368b36ffa2bfc2fba00d5bc1c6a7add5be1
EvelyneNamwoyo <namwoyoeve@gmail.com>
2018-09-19 14:48:54 +0300

    Modified functions

--- fastfood/fastfood_app/views.py
+++ fastfood/fastfood_app/views.py
@@ -1,26 +1,32 @@
 from fastfood_app import app
-from flask import jsonify, abort, make_response, request
+from flask import jsonify, make_response, request
 orders = [
-    {'id': 0,
-     'Category': 'Main Course',
-     'order status': False,
-     'Food Name':'Chicken Stew',
-     'Description': 'Nice and tasty food'},
-    {'id': 2,
-     'Category': 'Main Course',
-     'order status': False,
-     'Food Name':'Rice pilau',
-     'Description': 'Nice and tasty food'},
-    {'id': 3,
-     'Category': 'Drinks',
-     'order status': False,
-     'Food Name':'Mango Juice',
-     'Description': 'Made from the natural african mango'}
-]
+        {
+            'id': 0,
+            'Category': 'Main Course',
+            'order status': False,
+            'Food Name':'Chicken Stew',
+            'Description': 'Nice and tasty food'},
+       {
+           'id': 2,
+           'Category': 'Main Course',
+           'order status': False,
+           'Food Name':'Rice pilau',
+           'Description': 'Nice and tasty food'},
+       {
+           'id': 3,
+           'Category': 'Drinks',
+           'order status': False,
+           'Food Name':'Mango Juice',
+           'Description': 'Made from the natural african mango'}
+    ]
 
-@app.route('/FastFood/api/v2/orders', methods=['GET'])
+@app.route('/FastFood/api/v1/orders', methods=['GET'])
 def get_orders():
-    return jsonify({'orders':orders})
+    if len(orders)==0:
+        return make_response(jsonify({'message': 'There are no orders made'}), 404)
+
+    return jsonify({'orders':orders}), 200
 
 #This endpoint gets a specific order
 @app.route('/FastFood/api/v1/orders/<int:order_id>', methods=['GET'])
