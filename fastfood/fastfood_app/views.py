d7f24c218e8314a8e6a85389365c4f5a27e756c9
EvelyneNamwoyo <namwoyoeve@gmail.com>
2018-09-19 15:54:17 +0300

    Modify get_password function

--- fastfood/fastfood_app/views.py
+++ fastfood/fastfood_app/views.py
@@ -3,53 +3,82 @@ from flask import jsonify, abort, make_response, request
 from flask_httpauth import HTTPBasicAuth
 auth = HTTPBasicAuth()
 orders = [
-    {
-        'id': 1,
-        'category': 'Main Course',
-        'order status': False,
-        'Food Name':'Chicken Stew',
-        'Description': 'Nice and tasty food'},
-    {
-        'id': 2,
-        'category': 'Main Course',
-        'order status': False,
-        'Food Name':'Rice pilau',
-        'Description': 'Nice and tasty food'},
+        {
+            'id': 0,
+            'category': 'Main Course',
+            'order status': False,
+            'Food Name':'Chicken Stew',
+            'Description': 'Nice and tasty food'},
+       {
+           'id': 2,
+           'category': 'Main Course',
+           'order status': False,
+           'Food Name':'Rice pilau',
+           'Description': 'Nice and tasty food'},
+       {
+           'id': 3,
+           'category': 'Drinks',
+           'order status': False,
+           'Food Name':'Mango Juice',
+           'Description': 'Made from the natural african mango'}
+    ]
 
-    {   'id': 3,
-        'category': 'Drinks',
-        'order status': False,
-        'Food Name':'Mango Juice',
-        'Description': 'Made from the natural african mango'}
-]
-
-@app.route('/FastFood/api/v2/orders', methods=['GET'])
+@app.route('/FastFood/api/v1/orders', methods=['GET'])
 def get_orders():
-    return jsonify({'orders':orders})
+    if len(orders)==0:
+        return make_response(jsonify({'message': 'There are no orders made'}), 404)
+
+    return jsonify({'orders':orders}), 200
 
 #This endpoint gets a specific order
-@app.route('/FastFood/api/v2/orders/<int:order_id>', methods=['GET'])
+@app.route('/FastFood/api/v1/orders/<order_id>', methods=['GET'])
 def get_order(order_id):
-    order = [ order for order in orders if order['id'] == order_id]
-    if len(order) == 0:
-        jsonify({'error': 'Not found'}), 404
-    return jsonify({'order':order[0]})
+    if not order_id.isdigit():
+        return jsonify({'Error': 'Invalid order id, order id should be an integer'}),400
+    else:
+        
+        # order = [ order if order['id'] == order_id else 'That order has not yet been made' for order in orders ]
+        if len(orders) > 0:
+            item=[]
+            for order in orders:
+                if int(order['id']) == int(order_id):
+                    item.append(order)
+            if item == []:
+                return jsonify({'order':'That order has not yet been made'}),404
+            return jsonify({'order':item})
+        
+        else:
+            return make_response(jsonify({'error': 'There are no orders currently'}), 404)
 
 #Function for an endpoint to create new request
-@app.route('/FastFood/api/v2/orders', methods=['POST'])
+@app.route('/FastFood/api/v1/orders', methods=['POST'])
 def place_order():
-    if not request.json or not 'Food Name' in request.json:
-        abort(400)
-    order = {
-            'id':orders[-1]['id'] + 1,
-            'category':request.json['category'],
-            'order status': request.json['order status'],
-            'Food Name':request.json['Food Name'],
-            'Description':request.json['Description']
+    if not request.json :
+        return make_response(jsonify({'error_message': 'No parameters were passed'}), 404)
+    elif not 'Food Name' in request.json:
+        return make_response(jsonify({'error_message': 'Parameter Food Name missing'}), 404)
+        
+    elif not isinstance(request.json['Food Name'], str):
+        bad_request = {
+            "error": "Invalid order object",
+            "help_string":
+                "Request format should be {'category': 'Drinks or empty',"
+                "'Food Name': 'orange juice','Description': 'Mixture of lemon and tangerine' }"
             }
-    orders.append(order)
-    return jsonify({'order':order}), 201
+        return jsonify({'bad_request':bad_request}), 400
 
+    else:
+        order = {
+                'id':orders[-1]['id'] + 1,
+                'category':request.json.get('category', ""),
+                # 'category':request.json['category'],
+                'order status': False,
+                'Food Name':request.json['Food Name'],
+                'Description':request.json.get('Description', "")
+                # 'Description':request.json['Description']
+                }
+        orders.append(order)
+        return jsonify({'order':order}), 201
 
 @app.route('/FastFood/api/v1/orders/<int:order_id>', methods=['PUT'])
 @auth.login_required
