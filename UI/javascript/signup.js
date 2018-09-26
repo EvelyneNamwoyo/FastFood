898db2b2c30543942a21e18f65bd9cc9c40aba01
EvelyneNamwoyo <namwoyoeve@gmail.com>
2018-09-26 09:35:38 +0300

    Modified files

--- UI/javascript/signup.js
+++ UI/javascript/signup.js
@@ -1,5 +1,71 @@
 //Get the signup form modal
 signin = document.getElementById("")
+function logout_show(){
+    document.getElementById("logout").style.display = "block";
+
+}
+
+function logout_hide(){
+    document.getElementById("logout").style.display = "none";
+
+
+}
+
+function login_hide(){
+    document.getElementById("login").style.display = "none";
+
+}
+function account_hide(){
+    document.getElementById("account").style.display = "none";
+
+}
+function admin_hide(){
+    document.getElementById("admin").style.display = "none";
+
+}
+
+function login_show(){
+    document.getElementById("login").style.display = "block";
+
+}
+function account_show(){
+    document.getElementById("account").style.display = "block";
+
+}
+function admin_show(){
+    document.getElementById("admin").style.display = "block";
+
+}
+// function to display order histories
+function history_show(){
+    document.getElementById("history").style.display = "block";
+
+}
+// function to hide order histories
+function history_hide(){
+    document.getElementById("history").style.display = "none";
+
+}
+
+function logout(){
+    admin_show();
+    account_show();
+    login_show();
+    logout_show();
+    logout_hide();
+    history_hide();
+
+}
+
+function login(){
+    admin_hide();
+    account_hide();
+    login_hide();
+    logout_show();
+    signin_hide();
+    signup_hide();
+    history_show();
+}
 
 //functions to display signup form and signin form
 
@@ -38,4 +104,10 @@ function hide_chicken(){
 
 function hide_chips(){
     document.getElementById("chips").style.display = "none";
+}
+
+// The following functions enable addition and deletion of orders
+function delete_order(){
+    document.getElementById("item-1").style.display = "inline";
+    document.getElementById("order1").style.display = "none";
 }
