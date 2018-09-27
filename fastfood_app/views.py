d14360b834f86ac8a9d2ed671fddc60a4f0bcb63
EvelyneNamwoyo <namwoyoeve@gmail.com>
2018-09-27 16:33:40 +0300

    Add validation

--- fastfood_app/views.py
+++ fastfood_app/views.py
@@ -3,52 +3,45 @@ from flask import jsonify, abort, make_response, request
 from flask_httpauth import HTTPBasicAuth
 auth = HTTPBasicAuth()
 orders = [
-        {
-            'id': 1,
-            'category': 'Main Course',
-            'order status': False,
-            'Food Name':'Chicken Stew',
-            'Description': 'Nice and tasty food'},
-       {
-           'id': 2,
-           'category': 'Main Course',
-           'order status': False,
-           'Food Name':'Rice pilau',
-           'Description': 'Nice and tasty food'},
-       {
-           'id': 3,
-           'category': 'Drinks',
-           'order status': False,
-           'Food Name':'Mango Juice',
-           'Description': 'Made from the natural african mango'}
+    
     ]
 
 @app.route('/FastFood/api/v1/orders', methods=['GET'])
 def get_orders():
-    if len(orders)==0:
+    if not orders:
         return make_response(jsonify({'message': 'There are no orders made'}), 404)
-
     return jsonify({'orders':orders}), 200
+
+#This list stores the order available
+item=[]
+
+"""This function check whether the order id exists and if its an integer"""
+def valid_order_id(order_id):
+    if order_id.isdigit():
+        return True
+def available_orderid(order_id):
+    if len(orders) > 0:
+        for order in orders:
+            if int(order['id']) == int(order_id):
+                item.append(order)
+        if item:
+            return True
+
 
 #This endpoint gets a specific order
 @app.route('/FastFood/api/v1/orders/<order_id>', methods=['GET'])
 def get_order(order_id):
-    if not order_id.isdigit():
-        return jsonify({'Error': 'Invalid order id, order id should be an integer'}),400
+    # if not orders:
+    #     return make_response(jsonify({'error': 'There are no orders currently'}), 404)
+    if not valid_order_id(order_id):
+        return make_response(jsonify({'Error': 'Invalid order id, order id should be an integer'}),400)
+    elif  not available_orderid(order_id):
+        return make_response(jsonify({'order':'That order has not yet been made'}),404)
     else:
+        return jsonify({'order':item})
         
-        # order = [ order if order['id'] == order_id else 'That order has not yet been made' for order in orders ]
-        if len(orders) > 0:
-            item=[]
-            for order in orders:
-                if int(order['id']) == int(order_id):
-                    item.append(order)
-            if item == []:
-                return jsonify({'order':'That order has not yet been made'}),404
-            return jsonify({'order':item})
-        
-        else:
-            return make_response(jsonify({'error': 'There are no orders currently'}), 404)
+    
+    
 
 #Function for an endpoint to create new request
 @app.route('/FastFood/api/v1/orders', methods=['POST'])
@@ -58,7 +51,7 @@ def place_order():
     elif not 'Food Name' in request.json:
         return make_response(jsonify({'error_message': 'Parameter Food Name missing'}), 404)
         
-    elif not isinstance(request.json['Food Name'], str):
+    elif not isinstance(request.json['Food Name'],str):
         bad_request = {
             "error": "Invalid order object",
             "help_string":
@@ -66,31 +59,64 @@ def place_order():
                 "'Food Name': 'orange juice','Description': 'Mixture of lemon and tangerine' }"
             }
         return jsonify({'bad_request':bad_request}), 400
+    elif request.json['Food Name'].strip() == "":
+        return make_response(jsonify({'error_message': 'Food Name valus cannot be empty'}), 404)
 
     else:
-        order = {
-                'id':orders[-1]['id'] + 1,
-                'category':request.json.get('category', ""),
-                # 'category':request.json['category'],
-                'order status': False,
-                'Food Name':request.json['Food Name'],
-                'Description':request.json.get('Description', "")
-                # 'Description':request.json['Description']
+        if not orders:
+            order = {
+                    'id':1,
+                    'category':request.json.get('category', ""),
+                    'order status': False,
+                    'Food Name':request.json['Food Name'],
+                    'Description':request.json.get('Description', "")
                 }
-        orders.append(order)
-        return jsonify({'order':order}), 201
+            orders.append(order)
+            return jsonify({'order':order}), 201   
+        else:
+            order = {
+                    'id':orders[-1]['id'] + 1,
+                    'category':request.json.get('category', ""),
+                    'order status': False,
+                    'Food Name':request.json['Food Name'],
+                    'Description':request.json.get('Description', "")
+                }
+            orders.append(order)
+            return jsonify({'order':order}), 201
+def invalid_order(order_request):
+    if not order_request:
+        return True
+    if('category' in order_request and (type(order_request['category']) != str or order_request['category'].strip() == "")):
+        return True
+    if('order status' in order_request and (type(order_request['order status']) != str or order_request['order status'].strip() =="")):
+        return True
+    if('Food Name' in order_request and (type(request.json['Food Name']) != str or order_request['Food Name'].strip() == "")):
+        return True
+    if('Description' in request.json and (type(request.json['Description']) != str or order_request['Description'].strip() == "" or order_request['Description'].strip() == "")):
+        return True
 
-@app.route('/FastFood/api/v1/orders/<int:order_id>', methods=['PUT'])
+
+@app.route('/FastFood/api/v1/orders/<order_id>', methods=['PUT'])
 @auth.login_required
 def update_order_status(order_id):
-    order = [ order for order in orders if order['id'] == order_id]
-    if ((len(order)==0) or (not request.json) or ('category' in request.json and type(request.json['category']) != str) or ('order status' in request.json and type(request.json['order status']) != str) or ('Food Name' in request.json and type(request.json['Food Name']) != str) or ('Description' in request.json and type(request.json['Description']) != str)):
-        jsonify({'error': 'Invalid innput'}), 404
-    order[0]['category'] = request.json.get('category', order[0]['category'])
-    order[0]['order status'] = request.json.get('order status', order[0]['order status'])
-    order[0]['Food Name'] = request.json.get('Food Name', order[0]['Food Name'])
-    order[0]['Description'] = request.json.get('Description', order[0]['Description'])
-    return jsonify({'order':order[0]})
+    if not orders:
+        return make_response(jsonify({'No orders': 'You cannot make any updates'}), 404)
+    if invalid_order(request.json):
+        return jsonify({'error': 'Invalid input or missing value'}), 404
+    elif not valid_order_id(order_id):
+        return jsonify({'Error': 'Invalid order id, order id should be an integer'}),400
+    elif not available_orderid(order_id):
+        return jsonify({'No order':'That order has not yet been made, no possible updates'}),404
+    else:
+        item[0]['category'] = request.json.get('category', item[0]['category'])
+        item[0]['order status'] = request.json.get('order status', item[0]['order status'])
+        item[0]['Food Name'] = request.json.get('Food Name', item[0]['Food Name'])
+        item[0]['Description'] = request.json.get('Description', item[0]['Description'])
+        return jsonify({'order':item[0]}), 200
+        
+    
+    
+    
 
 @auth.get_password
 def get_password(username):
@@ -105,14 +131,14 @@ def unauthorized():
 @app.route('/FastFood/api/v1/orders/<int:order_id>', methods=['DELETE'])
 @auth.login_required
 def delete_order(order_id):
-    if len(orders) == 0:
-        return make_response(jsonify({'error': 'There are no orders currently'}), 404)
+    if not orders:
+        return make_response(jsonify({'error': 'There are no orders'}), 404)
     else:
         id_orders = []
         for order in orders:
              if order['id'] == order_id:
                 id_orders.append(order_id)
                 orders.remove(order)
-        if id_orders ==[]:
+        if not id_orders:
             return make_response(jsonify({'error': 'That order does not exist'}), 404)
         return make_response(jsonify({'Message': 'order deleted'}), 200)
--- test_api_endpoints.py
+++ test_api_endpoints.py
@@ -6,44 +6,64 @@ from base64 import b64encode
 class EndpointTest(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client()
-
-        
+        self.url = '/FastFood/api/v1/orders'
     def tearDown(self):
         pass
-    
+
     def test_to_get_all_orders(self):
-        response = self.app.get('/FastFood/api/v1/orders')
+        response = self.app.get(self.url)
         self.assertEqual(response.status_code, 200)
 
     """These tests check for different status codes and responses for all possible order_id inputs"""    
 
     def test_to_fetch_aspecific_order(self):
-        response = self.app.get('/FastFood/api/v1/orders/2')
-        self.assertEqual(response.status_code,200)
+        response = self.app.post(self.url, data = json.dumps({"Food Name": "beans"}), 
+                                content_type="application/json", follow_redirects=True)
+        self.assertEqual(response.status_code,201)
+        result = json.loads(response.data)
+        self.assertEqual(result, {'order':{
+                                'id':2,
+                                'category': '',
+                                'order status': False,
+                                'Food Name':'beans',
+                                'Description': ''
+                                    }
+                                })
+
+
 
     def test_to_throw_type_error_when_str_ispassed_as_id(self):
-        order_id = "1"
-        api_url = '/FastFood/api/v1/orders/'+ order_id
+        order_id = "/d"
+        api_url = self.url + order_id
         response = self.app.get(api_url)
         self.assertRaises(TypeError, response)
 
     def test_to_fetch_order_when_str_ispassed_as_id(self):
-        order_id = "1"
-        api_url = '/FastFood/api/v1/orders/'+ order_id
+        order_id = "/d"
+        api_url = self.url + order_id
         response = self.app.get(api_url)
-        self.assertEqual(response.status_code, 404)
+        result = json.loads(response.data)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(result,{"Error":"Invalid order id, order id should be an integer"})
 
     def test_to_fetch_aspecific_order_when_nonexistant_is_ispassed(self):
-        response = self.app.get('/FastFood/api/v1/orders/9')
+        response = self.app.get(self.url + '/9')
+        result=json.loads(response.data)
         self.assertEqual(response.status_code,404)
+        self.assertEqual(result,{"order":"That order has not yet been made"} )
+
         
     """This function checks whether api can make an order-post"""
     def test_to_place_order(self):
-        response = self.app.post('/FastFood/api/v1/orders',data = json.dumps({"Food Name": "milk"}), 
+        response = self.app.post(self.url, data = json.dumps({"Food Name": "milk"}), 
                                 content_type="application/json", follow_redirects=True)
+        self.app.post(self.url,data = json.dumps({"Food Name": "chocolate"}), 
+                                content_type="application/json", follow_redirects=True)
+
         result = json.loads(response.data)
+
         self.assertEqual(result, {'order':{
-                                'id':4,
+                                'id':3,
                                 'category': '',
                                 'order status': False,
                                 'Food Name':'milk',
@@ -54,13 +74,14 @@ class EndpointTest(unittest.TestCase):
     
     """This test function checks if mandatory parameter Food Name is not passed"""
     def test_for_mandatory_parameter_missing_in_placed_order(self):
-        response = self.app.post('/FastFood/api/v1/orders',data = json.dumps({"Description": "Tasty food"}), 
+        response = self.app.post(self.url,data = json.dumps({"Description": "Tasty food"}), 
                                 content_type="application/json", follow_redirects=True)
         self.assertEqual(response.status_code,404)
+        self.assertEqual(json.loads(response.data),{"error_message":"Parameter Food Name missing"})
 
     """This test function check whether Food Name parameter is a string"""
     def test_to_throw_type_error_when_str_is_notpassed_as_parameter(self):
-        response = self.app.post('/FastFood/api/v1/orders',data = json.dumps({"Food Name": 4}), 
+        response = self.app.post(self.url,data = json.dumps({"Food Name": 4}), 
                                 content_type="application/json", follow_redirects=True)
         self.assertRaises(TypeError, response)
 
@@ -71,18 +92,47 @@ class EndpointTest(unittest.TestCase):
         headers = {
                    'Authorization': 'Basic %s' % b64encode(b"admin:Eva").decode("ascii")
                    }
-        response = self.app.put('/FastFood/api/v1/orders/2',headers=headers, data = json.dumps({"order status": "Ready"}),
+        response = self.app.put(self.url + '/1',headers=headers, data = json.dumps({"order status": "Ready"}),
                                 content_type="application/json", follow_redirects=True)
         self.assertEqual(response.status_code,200)
+        result = json.loads(response.data)
 
-    """This test function checks whether status has not been updated incase no data was passed data"""
-    def test_for_not_updated(self):
+        self.assertEqual(result, {'order':{
+                                'id':1,
+                                'category': '',
+                                'order status': 'Ready',
+                                'Food Name':'chicken',
+                                'Description': ''
+                                    }
+                                })
+
+    
+
+    '''This function checks for empty values'''
+    def test_for_missing_value_when_updating(self):
         headers = {
                    'Authorization': 'Basic %s' % b64encode(b"admin:Eva").decode("ascii")
                    }
-        response = self.app.put('/FastFood/api/v1/orders/2',headers=headers, data = json.dumps({}),
+
+        self.app.post(self.url, data = json.dumps({"Food Name": "chicken"}), 
                                 content_type="application/json", follow_redirects=True)
-        self.assertEqual(response.status_code,200)
+        
+        response = self.app.put( self.url +'/1', headers=headers, data = json.dumps({"Food Name":""}),
+                                content_type="application/json", follow_redirects=True)
+        self.assertEqual(json.loads(response.data), {"error":"Invalid input or missing value"})
+
+        self.assertEqual(response.status_code,404)
+
+    """This function tests for an attempt to update an unavailable order ids"""
+    def test_non_existing_order_ids(self):
+        headers = {
+                   'Authorization': 'Basic %s' % b64encode(b"admin:Eva").decode("ascii")
+                   }
+        response = self.app.put('/FastFood/api/v1/orders/9',headers=headers, data = json.dumps({"Food Name":"goat"}),
+                                content_type="application/json", follow_redirects=True)
+        self.assertEqual(json.loads(response.data), {"No order":"That order has not yet been made, no possible updates"})
+
+        self.assertEqual(response.status_code,404)
 
     """This tests checks whether access denied message is returned for un authorized users"""
     def test_for_access_denied_for_unauthorized_users(self):
@@ -95,8 +145,10 @@ class EndpointTest(unittest.TestCase):
         headers = {
                    'Authorization': 'Basic %s' % b64encode(b"admin:Eva").decode("ascii")
                    }
-        response = self.app.delete('/FastFood/api/v1/orders/1',headers=headers)
-        self.assertEqual(response.status_code,200)
+       
+        response = self.app.delete('/FastFood/api/v1/orders/4',headers=headers)
+        self.assertEqual(response.status_code,404)
+        self.assertEqual(json.loads(response.data), {'error': 'That order does not exist'})
 
 
 
